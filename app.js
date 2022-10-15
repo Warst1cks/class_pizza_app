de3a1d6b57e05da970d116f5b8f5ff87f04d2ff8
Samuel Warrie <samuelwarrie735@gmail.com>
2022-10-15 23:07:51 +0100

    modified my app.js

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require("express");
-
-const userRouter = require("./routes/userRoutes");
-const orderRouter = require("./routes/orderRoutes");
-
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const userRouter = require("../class_pizza_app/routes/userRoutes");
+const orderRouter = require("../class_pizza_app/routes/orderRoutes");
+require("dotenv").config();
+require("../class_pizza_app/authentication/auth");
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/users", userRouter);
-app.use("/orders", orderRouter);
+app.use("/users", passport.authenticate("jwt", { session: false }), userRouter);
+app.use(
+  "/orders",
+  passport.authenticate("jwt", { session: false }),
+  orderRouter
+);
 
 module.exports = app;
